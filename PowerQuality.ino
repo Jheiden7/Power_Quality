91f5ace47be1e884755a7a060fe82aa1856558af
Jhonny Ibarbo <104173510+Jheiden7@users.noreply.github.com>
2024-05-16 02:11:23 -0500

    Organización del código por funciones, extracción y almacenamiento por CSV, obtención de fecha y hora actual.

--- PowerQuality.ino
+++ PowerQuality.ino
@@ -2,6 +2,8 @@
 #include <ArduinoJson.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 // Credenciales para conexión WiFi
 const char *ssid = "ProyectosElectronica"; // Nombre de la red WiFi.
@@ -18,20 +20,32 @@ const int mqtt_port = 1883; // Puerto de comunicación.
 float pot_aparente;
 float pot_reactiva;
 
+// Variables para extraer datos de fecha y tiempo.
+String date;
+String Time;
+String dateTime;
+
+
 #if defined(ESP32)
-PZEM004Tv30 pzem(Serial2, 16, 17); RX2, TX2
+PZEM004Tv30 pzem(Serial2, 16, 17); // RX2, TX2
 #else
 PZEM004Tv30 pzem(Serial2);
 #endif
 
 // Variables para el manejo de formato JSON.
-String jsonSerial = ""; 
+String jsonSerial = "";
 JsonDocument myjson; //Se genera un objeto JSON.
 char msg;
+
+unsigned long previousTime = 0; // Variable para contar segundos.
 
 // Clientes para publicación a MQTT.
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+// Objetos para acceder al servidor NTP y obtener la fecha y hora.
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
 void setup() {
   Serial.begin(115200);
@@ -65,6 +79,8 @@ void setup() {
       delay(2000);
     }
   }
+  timeClient.begin(); // Se da inicio al cliente NTP.
+  timeClient.setTimeOffset(-18000); // Se establece un desfase -18000 segundos para sincronizar con la ubicación geográfica de Colombia.
 }
 
 // Función para procesar los mensajes recibidos en un tópico MQTT.
@@ -80,7 +96,9 @@ void callback(char *topic, byte *payload, unsigned int length) {
 }
 
 void loop() {
-        
+
+  unsigned long currentTime = millis(); // Se obtienen los segundos que han pasado.
+
   Serial.print("Custom Address:");
   Serial.println(pzem.readAddress(), HEX);
 
@@ -95,6 +113,33 @@ void loop() {
   // Se obtienen variables faltantes que no se extraen directamente desde el sensor
   pot_aparente = abs(voltage)*abs(current); // Se obtiene la potencia aparente - VA
   pot_reactiva = pot_aparente*sin(acos(pf)); // Se obtiene la potencia reactiva - VAr
+
+  impresionSerial(voltage, current, power, energy, pf, frequency, pot_aparente, pot_reactiva); // Impresión de las variables.
+  crearJson(voltage, current, power, energy, pf, frequency, pot_aparente, pot_reactiva); // Creación del JSON.
+  
+
+  serializeJson(myjson, jsonSerial); // Se serializa (conversión a string) el contenido del JSON para facilitar su transmisión.
+
+  // Conversión de string a char para realizar la publicación al tópico MQTT.
+  char json_serial[jsonSerial.length()+1];
+  jsonSerial.toCharArray(json_serial, jsonSerial.length()+1);
+
+  // Cada 10 segundos, se obtiene la fecha y se imprime el CSV.
+  if(currentTime - previousTime >= 10000){
+    previousTime = currentTime;
+
+    dateTime = getDatetime();
+    serialCSV(voltage, current, power, energy, pf, frequency, pot_aparente, pot_reactiva, dateTime);
+  }
+
+  client.loop(); // Revisa constantemente si hay nuevos mensajes.
+  client.publish(topic, json_serial); // Se publica el JSON serializado al tópico MQTT.
+
+  delay(1000);
+}
+
+// Función para imprimir las variables eléctricas en el monitor serial.
+void impresionSerial(float voltage, float current, float power, float energy, float pf, float frequency, float pot_aparente, float pot_reactiva){
 
   // Se verifica si se pudo obtener todas las variables, en caso de que no, mostrará un error de lectura.
   if(isnan(voltage)){
@@ -121,29 +166,57 @@ void loop() {
     Serial.print("PF: ");                Serial.println(pf);
     Serial.print("Reactive power: ");    Serial.print(pot_reactiva, 1);     Serial.println("VAr");
     Serial.print("Apparent power: ");    Serial.print(pot_aparente, 1);     Serial.println("VA");
-
-    // Se almacenan las variables en un objeto JSON de forma llave-valor.
-    myjson["Voltage"] = voltage;
-    myjson["Current"] = current;
-    myjson["Power"] = power;
-    myjson["Energy"] = energy;
-    myjson["Frequency"] = frequency;
-    myjson["PF"] = pf;
-    myjson["Reactive"] = pot_reactiva;
-    myjson["Aparent"] = pot_aparente;
-
   }
+}
 
-  serializeJson(myjson, jsonSerial); // Se serializa (conversión a string) el contenido del JSON para facilitar su transmisión.
-  Serial.println();
-  Serial.println(jsonSerial);
-  Serial.println();
+// Función para realizar el empaquetado JSON.
+void crearJson(float voltage, float current, float power, float energy, float pf, float frequency, float pot_aparente, float pot_reactiva){
 
-  // Conversión de string a char para realizar la publicación al tópico MQTT.
-  char avr[jsonSerial.length()+1];
-  jsonSerial.toCharArray(avr, jsonSerial.length()+1);
+  // Se almacenan las variables en un objeto JSON de forma llave-valor.
+  myjson["Voltage"] = voltage;
+  myjson["Current"] = current;
+  myjson["Power"] = power;
+  myjson["Energy"] = energy;
+  myjson["Frequency"] = frequency;
+  myjson["PF"] = pf;
+  myjson["Reactive"] = pot_reactiva;
+  myjson["Aparent"] = pot_aparente;
 
-  client.loop(); // Revisa constantemente si hay nuevos mensajes.
-  client.publish(topic, avr); // Se publica el JSON serializado al tópico MQTT.
-  delay(500);
+}
+
+// Función para extraer la fecha y hora actual.
+String getDatetime(){
+  timeClient.update(); // Se actualiza el cliente al momento actual.
+  
+  date = timeClient.getFormattedDate(); // Se extrae la fecha.
+  Time = timeClient.getFormattedTime(); // Se extrae la hora.
+  int split = date.indexOf("T"); // Se obtiene el índice donde termina la fecha.
+
+  date = date.substring(0, split); // Se extrae únicamente la fecha en formato YYYY-MM-DD
+  
+  String datetime = date + " " + Time; // Se concatena la fecha y hora, obtieniendo un datetime con formato YYYY-MM-DD HH-MM-SS
+  
+  return datetime;
+}
+
+// Función para una impresión serial de las variables en un formato CSV, esto para realizar la extracción y almacenamiento a un archivo de texto.
+void serialCSV(float voltage, float current, float power, float energy, float pf, float frequency, float pot_aparente, float pot_reactiva, String date){
+  Serial.print(date);
+  Serial.print(",");
+  Serial.print(voltage);
+  Serial.print(",");
+  Serial.print(current);
+  Serial.print(",");
+  Serial.print(power);
+  Serial.print(",");
+  Serial.print(energy);
+  Serial.print(",");
+  Serial.print(frequency);
+  Serial.print(",");
+  Serial.print(pf);
+  Serial.print(",");
+  Serial.print(pot_reactiva);
+  Serial.print(",");
+  Serial.print(pot_aparente);
+  Serial.print("\n");
 }
